0c1a43da05a9b35fdd7543927bca9eb2d2f74ba0
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-24 23:58:48 +0000

    commented out tests for fetch my IP, implemented one for  fetch coords by IP

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+// used for testing fetch my IP
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   } else if (ip) {
+//     console.log('It worked! Returned IP:', ip);
+//   }
+// });
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  } else if (ip) {
-    console.log('It worked! Returned IP:', ip);
-  }
+fetchCoordsByIP('142.179.2.174', (error, data) => {
+  console.log(error, data);
 });
