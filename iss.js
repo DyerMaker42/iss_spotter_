b622ce987400e092573862c3fda87743cb387dd9
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-25 01:06:01 +0000

    added inital logic for pass finder

--- iss.js
+++ iss.js
@@ -48,6 +48,34 @@ const fetchCoordsByIP = (ip, callback) => {
   });
 };
 
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+  // ...
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const passArray = JSON.parse(body)
+    console.log(passArray)
+    // console.log(body)
+    //console.log(data.ip)
+    callback(null, ip);
+  });
+};
 
 
 
