94198e18637fbef209168afba851ca91b58af68e
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-11-07 20:35:46 +0000

    passed ip to ipvigilate

--- iss_promised.js
+++ iss_promised.js
@@ -4,8 +4,8 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json')
 }
 
-const fetchCoordsByIP = function(ip){
-  const IP = JSON.parse(ip)["ip"]
-  console.log(IP)
+const fetchCoordsByIP = function(body){
+  const IP = JSON.parse(body).ip
+  return request(`https://ipvigilante.com/json/${ip}`)
 }
 module.exports = {fetchMyIP, fetchCoordsByIP}
