94d248a1a1775d44f1ca068f4b250c5a4250d838
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-11-07 22:28:26 +0000

    updated and finished

--- index2.js
+++ index2.js
@@ -1,4 +1,15 @@
-const {fetchMyIP} = require('./iss_promised');
+//const {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, } = require('./iss_promised');
+const {nextISSTimesForMyLocation} = require('./iss_promised');
+// fetchMyIP()
+// .then(fetchCoordsByIP)
+// .then(fetchISSFlyOverTimes)
+// .then(body => console.log(body))
+// .catch (res => console.log(res, "this error"));
 
-fetchMyIP()
-.then(body => console.log(body));
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
--- iss_promised.js
+++ iss_promised.js
@@ -4,8 +4,28 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json')
 }
 
-const fetchCoordsByIP = function(body){
-  const IP = JSON.parse(body).ip
-  return request(`https://ipvigilante.com/json/${ip}`)
-}
-module.exports = {fetchMyIP, fetchCoordsByIP}
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  //console.log(ip,"ip")
+  return request(`https://api.ipgeolocationapi.com/geolocate/${ip}`);
+};
+
+
+const fetchISSFlyOverTimes = function(body) {
+  const {latitude, longitude } = JSON.parse(body).geo;
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+//onst fetchISSFlyOverTimes ()
+//module.exports = {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes}
+module.exports={nextISSTimesForMyLocation}
